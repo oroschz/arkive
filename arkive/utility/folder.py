7243c8882754e39ee752d6c4ec0b0e7b005d6d05
Orlando Ospino Sánchez <oroschz@outlook.com>
2021-03-11 23:29:38 -0500

    feat: added folder utility functions.

---
+++ arkive/utility/folder.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+
+def folder_files(folder: Path, recurse=False):
+    glob = folder.rglob('*') if recurse else folder.glob('*')
+    for item in glob:
+        if item.is_file():
+            yield item
+
+
+def folder_remove(folder: Path):
+    try:
+        folder.rmdir()
+    except OSError as err:
+        print(err)
+
+
+def folder_cleanup(folder: Path):
+    for item in folder.glob("*"):
+        if item.is_dir():
+            folder_cleanup(item)
+            folder_remove(item)
