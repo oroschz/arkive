f526ecadf776dfdbab6dd1cf2e803d043914b42a
Orlando Ospino Sánchez <oroschz@outlook.com>
2021-04-02 01:43:01 -0500

    feat: added logging to pcloud drive module.

--- arkive/drives/pcloud.py
+++ arkive/drives/pcloud.py
@@ -1,8 +1,11 @@
 import json
+import logging
 from urllib import request, parse
 from pathlib import Path
 
 from arkive.core.drive import Drive
+
+logger = logging.getLogger(__name__)
 
 
 def _pcloud_metadata(track: dict):
@@ -10,6 +13,7 @@ def _pcloud_metadata(track: dict):
 
 
 def _pcloud_recurse(root: dict, path: Path):
+    logger.debug(f'Checking contents of folder "{path.as_posix()}".')
     for item in root.get('contents', []):
         item['path'] = path / item['name']
         if not item['isfolder'] and item['icon'] == 'audio':
@@ -18,32 +22,48 @@ def _pcloud_recurse(root: dict, path: Path):
 
 
 def _pcloud_request(action: str, params: dict, auth: dict):
+    logger.debug(f'Sending request for {action.upper()}.')
     queries = parse.urlencode({**params, **auth})
     response = request.urlopen(f'https://api.pcloud.com/{action}?{queries}')
-    return json.load(response)
+    data = json.load(response)
+    logger.debug(f'Received response for {action.upper()} with status {data["result"]}.')
+    return data
 
 
 def _pcloud_index(path: Path, auth: dict):
     params = {'path': path.as_posix(), 'recursive': True}
     data = _pcloud_request('listfolder', params, auth)
-    assert data['result'] is 0, f'STATUS {data["result"]}: {data["error"]}'
-    yield from _pcloud_recurse(data['metadata'], path)
+    if data['result'] == 0:
+        yield from _pcloud_recurse(data['metadata'], path)
+        logger.info(f'Indexing completed on folder "{path.as_posix()}".')
+    else:
+        logger.error(f'Failed to index folder "{path.as_posix()}".')
+        raise Exception(data['error'])
 
 
 def _pcloud_rename(source: Path, dest: Path, auth: dict):
     params = {'path': source.as_posix(), 'topath': dest.as_posix()}
     data = _pcloud_request('renamefile', params, auth)
-    assert data['result'] is 0, f'STATUS {data["result"]}: {data["error"]}'
+    if data['result'] == 0:
+        logger.info(f'Renamed file to ".../{dest.name}".')
+    else:
+        logger.warning(f'Failed to rename ".../{source.name}".')
+        raise Exception(data['error'])
 
 
 def _pcloud_create_folder(folder: Path, auth: dict):
+    logger.debug(f'Creating folder "{folder.as_posix()}".')
     params = {'path': folder.as_posix()}
     data = _pcloud_request('createfolderifnotexists', params, auth)
-    if data['result'] == 2002:
+    if data['result'] == 0:
+        logger.debug(f'Created folder "{folder.as_posix()}".')
+    elif data['result'] == 2002:
+        logger.debug(f'Failed creating folder "{folder.as_posix()}".')
         _pcloud_create_folder(folder.parent, auth)
         _pcloud_create_folder(folder, auth)
     else:
-        assert data['result'] is 0, f'STATUS {data["result"]}: {data["error"]}'
+        logger.warning(f'Failed to create folder "{folder.as_posix()}".')
+        raise Exception(data['error'])
 
 
 def _pcloud_list_items_recurse(root: dict, path: Path):
@@ -61,8 +81,15 @@ def _pcloud_list_items(path: Path, auth: dict):
 
 
 def _pcloud_remove_folder(folder: Path, auth: dict):
+    logger.info(f'Removing folder "{folder.as_posix()}".')
     params = {'path': folder.as_posix()}
-    _pcloud_request('deletefolder', params, auth)
+    data = _pcloud_request('deletefolder', params, auth)
+    if data['result'] == 0:
+        logger.info(f'Removed folder {folder.as_posix()}.')
+    elif data['result'] == 2006:
+        logger.debug(f'Failed to remove folder "{folder.as_posix()}". Folder is noy empty.')
+    else:
+        logger.info(f'Failed to remove folder "{folder.as_posix()}".')
 
 
 def _pcloud_cleanup(folder: Path, auth: dict):
@@ -77,11 +104,14 @@ class PCloudDrive(Drive):
         self.auth = auth
 
     def index(self, folder: Path):
+        logger.info(f'Indexing folder "{folder.as_posix()}".')
         yield from _pcloud_index(folder, self.auth)
 
     def rename(self, source: Path, dest: Path):
+        logger.info(f'Ensuring folder "{dest.parent.as_posix()}" exists.')
         _pcloud_create_folder(dest.parent, self.auth)
         _pcloud_rename(source, dest, self.auth)
 
     def cleanup(self, folder: Path):
+        logger.info(f'Cleaning up folder "{folder.as_posix()}".')
         _pcloud_cleanup(folder, self.auth)
