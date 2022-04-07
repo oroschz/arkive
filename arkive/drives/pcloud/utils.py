62dab3f65f22272de2d2945b7753b91173881db2
Orlando Ospino Sánchez <oroschz@outlook.com>
2022-04-07 12:26:16 -0500

    refactor: organize drives module structure.

--- arkive/drives/pcloud/utils.py
+++ arkive/drives/pcloud/utils.py
@@ -1,24 +1,23 @@
 import logging
 from pathlib import Path
 
-from arkive.core.drive import Drive
 from arkive.core.message import RestMessage
 from arkive.core.request import send_request
 
 logger = logging.getLogger(__name__)
 
 
-def _pcloud_metadata(track: dict):
+def get_track_metadata(track: dict):
     return {key: track[key] for key in ['artist', 'album', 'title', 'path']}
 
 
-def _pcloud_recurse(root: dict, path: Path):
+def drive_recourse(root: dict, path: Path):
     logger.debug(f'Checking contents of folder "{path.as_posix()}".')
     for item in root.get('contents', []):
         item['path'] = path / item['name']
         if not item['isfolder'] and item['icon'] == 'audio':
-            yield _pcloud_metadata(item)
-        yield from _pcloud_recurse(item, item["path"])
+            yield get_track_metadata(item)
+        yield from drive_recourse(item, item["path"])
 
 
 def create_message(action: str, auth: dict = None, params: dict = None) -> RestMessage:
@@ -29,19 +28,19 @@ def create_message(action: str, auth: dict = None, params: dict = None) -> RestM
     return message
 
 
-def _pcloud_index(path: Path, auth: dict):
+def drive_index(path: Path, auth: dict):
     params = {'path': path.as_posix(), 'recursive': True}
     message = create_message("listfolder", auth, params)
     data = send_request(message)
     if data['result'] == 0:
-        yield from _pcloud_recurse(data['metadata'], path)
+        yield from drive_recourse(data['metadata'], path)
         logger.info(f'Indexing completed on folder "{path.as_posix()}".')
     else:
         logger.error(f'Failed to index folder "{path.as_posix()}".')
         raise Exception(data['error'])
 
 
-def _pcloud_rename(source: Path, dest: Path, auth: dict):
+def drive_rename(source: Path, dest: Path, auth: dict):
     params = {'path': source.as_posix(), 'topath': dest.as_posix()}
     message = create_message("renamefile", auth, params)
     data = send_request(message)
@@ -52,7 +51,7 @@ def _pcloud_rename(source: Path, dest: Path, auth: dict):
         raise Exception(data['error'])
 
 
-def _pcloud_create_folder(folder: Path, auth: dict):
+def drive_mkdir(folder: Path, auth: dict):
     logger.debug(f'Creating folder "{folder.as_posix()}".')
     params = {'path': folder.as_posix()}
     message = create_message("createfolderifnotexists", auth, params)
@@ -61,29 +60,29 @@ def _pcloud_create_folder(folder: Path, auth: dict):
         logger.debug(f'Created folder "{folder.as_posix()}".')
     elif data['result'] == 2002:
         logger.debug(f'Failed creating folder "{folder.as_posix()}".')
-        _pcloud_create_folder(folder.parent, auth)
-        _pcloud_create_folder(folder, auth)
+        drive_mkdir(folder.parent, auth)
+        drive_mkdir(folder, auth)
     else:
         logger.warning(f'Failed to create folder "{folder.as_posix()}".')
         raise Exception(data['error'])
 
 
-def _pcloud_list_items_recurse(root: dict, path: Path):
+def drive_list_items_recourse(root: dict, path: Path):
     for item in root.get('contents', []):
         item['path'] = path / item['name']
         yield item
-        yield from _pcloud_list_items_recurse(item, item["path"])
+        yield from drive_list_items_recourse(item, item["path"])
 
 
-def _pcloud_list_items(path: Path, auth: dict):
+def drive_list_items(path: Path, auth: dict):
     params = {'path': path.as_posix(), 'recursive': False}
     message = create_message("listfolder", auth, params)
     data = send_request(message)
     assert data['result'] is 0, f'STATUS {data["result"]}: {data["error"]} -> {path}'
-    yield from _pcloud_list_items_recurse(data['metadata'], path)
+    yield from drive_list_items_recourse(data['metadata'], path)
 
 
-def _pcloud_remove_folder(folder: Path, auth: dict):
+def drive_rmdir(folder: Path, auth: dict):
     logger.info(f'Removing folder "{folder.as_posix()}".')
     params = {'path': folder.as_posix()}
     message = create_message("deletefolder", auth, params)
@@ -96,26 +95,8 @@ def _pcloud_remove_folder(folder: Path, auth: dict):
         logger.info(f'Failed to remove folder "{folder.as_posix()}".')
 
 
-def _pcloud_cleanup(folder: Path, auth: dict):
-    for item in _pcloud_list_items(folder, auth):
+def drive_cleanup(folder: Path, auth: dict):
+    for item in drive_list_items(folder, auth):
         if item['isfolder']:
-            _pcloud_cleanup(item['path'], auth)
-            _pcloud_remove_folder(item['path'], auth)
-
-
-class PCloudDrive(Drive):
-    def __init__(self, auth: dict):
-        self.auth = auth
-
-    def index(self, folder: Path):
-        logger.info(f'Indexing folder "{folder.as_posix()}".')
-        yield from _pcloud_index(folder, self.auth)
-
-    def rename(self, source: Path, dest: Path):
-        logger.info(f'Ensuring folder "{dest.parent.as_posix()}" exists.')
-        _pcloud_create_folder(dest.parent, self.auth)
-        _pcloud_rename(source, dest, self.auth)
-
-    def cleanup(self, folder: Path):
-        logger.info(f'Cleaning up folder "{folder.as_posix()}".')
-        _pcloud_cleanup(folder, self.auth)
+            drive_cleanup(item['path'], auth)
+            drive_rmdir(item['path'], auth)
